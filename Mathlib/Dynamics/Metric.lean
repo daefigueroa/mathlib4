2f93f6f639adb60ef955276372a7f8d075bbf93a
Daniel Figueroa <40865765+daefigueroa@users.noreply.github.com>
2025-11-05 07:55:01 +0100

    add Metric.lean

---
+++ Mathlib/Dynamics/Metric.lean
@@ -0,0 +1,17 @@
+/-
+Copyright (c) 2025 Daniel Figueroa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Daniel Figueroa
+-/
+import Mathlib.Dynamics.Transitive
+
+/-!
+# Monoid actions on metric spaces
+
+In this file we define distal monoid actions on metric spaces and prove basic facts about these
+actions.
+
+## Tags
+
+group action, metric space
+-/
